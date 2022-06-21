823323d12f65a55166a311b3ef2bdce74fadcd66
Eran Cohen <er11cohen@gmail.com>
2022-06-21 14:59:39 +0300

    Fix share button

--- rabiEliezer/src/main/java/com/eran/rabieliezer/MainActivity.java
+++ rabiEliezer/src/main/java/com/eran/rabieliezer/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends Activity {
     private ArrayList<Perek> alPerekFilter;
 
     String shareStr = "פרקי רבי אליעזר https://play.google.com/store/apps/details?id=com.eran.rabieliezer";
-    WeakReference<Activity> WeakReferenceActivity;
+    WeakReference<Activity> weakReferenceActivity;
 
     Boolean DrawerLayoutOpen = false;
     SearchView searchView;
@@ -142,7 +142,7 @@ public class MainActivity extends Activity {
             }
         });
 
-        WeakReferenceActivity = new WeakReference<Activity>(this);
+        weakReferenceActivity = new WeakReference<Activity>(this);
 
 
 //		String version = sharedPreferences.getString("version","-1");
@@ -193,17 +193,7 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
 
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
             getMenuInflater().inflate(R.menu.main, menu);
-
-            MenuItem item = menu.findItem(R.id.menu_item_share);
-            ShareActionProvider myShareActionProvider = (ShareActionProvider) item.getActionProvider();
-            Intent myIntent = new Intent();
-            myIntent.setAction(Intent.ACTION_SEND);
-            myIntent.putExtra(Intent.EXTRA_TEXT, shareStr);
-            myIntent.setType("text/plain");
-            myShareActionProvider.setShareIntent(myIntent);
-
 
             searchView = (SearchView) menu.findItem(R.id.menu_item_search).getActionView();
             searchView.setQueryHint("חיפוש במפתח");
@@ -241,7 +231,7 @@ public class MainActivity extends Activity {
                 }
 
             });
-        }
+
         return true;
     }
 
@@ -289,6 +279,9 @@ public class MainActivity extends Activity {
         switch (item.getItemId()) {
             case R.id.menu_item_score:
                 scoreInGooglePlay();
+                break;
+            case R.id.menu_item_share:
+                Utils.shareApp(weakReferenceActivity, shareStr);
                 break;
             default:
                 break;
@@ -358,13 +351,13 @@ public class MainActivity extends Activity {
     }
 
     public void OpenHelp() {
-        Utils.alertDialogShow(WeakReferenceActivity, getApplicationContext(),
+        Utils.alertDialogShow(weakReferenceActivity, getApplicationContext(),
                 "עזרה", android.R.drawable.ic_menu_help, "files/help.txt",
                 "הבנתי", "זכו את הרבים", shareStr);
     }
 
     public void OpenAbout() {
-        Utils.alertDialogShow(WeakReferenceActivity, getApplicationContext(),
+        Utils.alertDialogShow(weakReferenceActivity, getApplicationContext(),
                 "אודות", android.R.drawable.ic_menu_info_details,
                 "files/about.txt", "אשריכם תזכו למצוות", "זכו את הרבים",
                 shareStr);
